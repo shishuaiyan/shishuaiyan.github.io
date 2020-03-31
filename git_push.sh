33c58d302489f1ec4ef62ab641b85970bc4e3ff7
shishuai.yan <yan_ss@qq.com>
2020-03-31 14:52:44 +0800

    update

--- git_push.sh
+++ git_push.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+echo ">>> git pull"
 git pull
+echo ">>> git add ."
 git add .
+echo ">>> git status"
 git status
 read -p "Commit? [Y]/N: " flag
 case $flag in
     [yY]|"$null")
         echo "yes"
+        echo ">>> git commit -m \"update\""
         git commit -m "update"
+        echo ">>> git push"
         git push
         ;;
     *)
         echo "no"
+        echo ">>> git reset HEAD"
         git reset HEAD
 esac
+echo ">>> git status"
 git status
